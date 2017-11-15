c282ce1517b337806caaed96618cdf92cc0d44f4
Guzel Nasybullina <gnasybullina@hawk.iit.edu>
2017-11-15 15:44:05 -0600

    Create s3test.php

---
+++ s3test.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>php test page</title>
+</head>
+<body>
+
+ <?php
+        echo "Hello World";
+echo $image_url("https://s3-us-west-2.amazonaws.com/raw-gnr/sky.jpg");
+?>
+<img src="<?php echo $image_url>">
+</body>
+</html>
